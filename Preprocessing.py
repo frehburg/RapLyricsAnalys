3320f081467dfc5373973a83715371acf4ee6b70
Filip Rehburg <fil.rehburg@gmail.com>
2023-03-27 16:14:42 +0200

    Added preprocessing module WIP

---
+++ Preprocessing.py
@@ -0,0 +1,23 @@
+def create_rap_df():
+    # load in top50_2018_2022.csv
+    # this csv has a column with the lyrics in one string
+    
+    # apply preprocess function to each list in the lyrics column
+
+    # call create_word_count_dict
+
+    pass
+
+    # output
+
+
+def preprocess(list_of_words):
+    # write alternatives for format list_of_words = ['a', 'b', 'c'] or df['col']
+    # 1. split the lyrics into a list of words
+    # 2. lemmatization to convert words to their root form
+    # IMPORTANT: remove punctuation but leave in apostrophes
+    pass
+
+def create_word_count_dict(df):
+    # create a dictionary of words and their counts and their relative frequencies
+    pass
